c8e8cf60163773cc3ba69cf73e8c54ef961642bf
Bengy Phan <bengyphan@gmail.com>
2018-10-18 17:39:00 -0700

    third commit

--- js/main.js
+++ js/main.js
@@ -22,11 +22,24 @@ console.log(popNum())
 
 // 7
 
+// function addZero(i) {
+// 	if(i < 10) {
+// 		return "0" + i
+// 	} else {
+// 		return i
+// 	}
+// }
+
+
 function getDate() {
 	var date = new Date()
 	var hour = date.getHours()
 	var min = date.getMinutes()
 	var sec = date.getSeconds()
+	var month = date.getMonth() + 1
+	var day = date.getDate()
+	var year = date.getFullYear()
+
 
 	if(hour < 10){
 		hour = "0" + hour
@@ -37,7 +50,23 @@ function getDate() {
 	if(sec < 10) {
 		sec = "0" + sec
 	}
-	var showDate = hour + ": " + min + ": " + sec
-	document.body.innerHTML = "<h1>" + showDate +"</h1>"
+	if(month < 10) {
+		month = "0" + month
+	}
+	if(day < 10) {
+		day = "0" + day
+	}
+
+
+	var displayDate = month + "/ " + day + "/ " + year
+	var showTime = hour + ": " + min + ": " + sec
+
+
+	document.body.innerText = displayDate + " " + showTime;
+
+	setTimeout(function() {
+		getDate();
+	},1000)
 }
-console.log(getDate());
+
+getDate();
